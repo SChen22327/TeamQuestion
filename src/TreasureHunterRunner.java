6c715a757782ef6f706e7e331d9fe13703bed68c
BT_1N3_16 <sample@bths.edu>
2023-12-18 08:39:07 -0500

    test commit

--- src/TreasureHunterRunner.java
+++ src/TreasureHunterRunner.java
@@ -1,6 +1,7 @@
 public class TreasureHunterRunner {
     public static void main(String[] args) {
         System.out.println("Stanford");
+        System.out.println("Jason");
         TreasureHunter game = new TreasureHunter();
         game.play();
     }
